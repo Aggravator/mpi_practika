f281d9b2a84ca39affe40cdc3b01a4694755bcac
Aggravator <phortopiaono@mail.ru>
2014-11-13 01:03:09 -0800

    Last version

--- BroadcastMPI/BroadcastMPI/main.cpp
+++ BroadcastMPI/BroadcastMPI/main.cpp
@@ -1,53 +1,36 @@
 #include "mpi.h"
-#include <cmath>
 #include <stdio.h>
 const int maxBuff=200;
 int size, rank;
-int getPseudoRank(int root, int rank,int size){
-	return (rank+(size-root))%size;
-}
-int getRealRank(int root,int prank,int size){
-	return (prank+root)%size;
-}
-int getIteration(int prank){
-	int lastnode=1;
-	int iter=0;
-	while(lastnode<prank)lastnode+=floor(pow(2.0,iter++));
+int getIteration(const int prank){
+	int lastnode=1,iter=1;
+	for(lastnode;lastnode<prank;lastnode<<=1)++iter;
 	return iter;
-}
-int getDonorRank(int prank){
-	int iter=getIteration(prank);
-	return prank-int(pow(2.0,iter-1));
 }
 int MyBroadcastSend(void *buffer, int count, MPI_Datatype datatype, int root){
 	MPI_Status status;
-	int prank=getPseudoRank(root,rank,size);
-	int lastnode= root==rank?1:rank;
+	int prank=(rank+(size-root))%size;
 	int iter=0;
 	if(root!=rank){
-		printf("I %d wait message from %d\n",rank,getRealRank(root,getDonorRank(prank),size));
-		MPI_Recv(buffer,count,datatype, MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-		printf("I %d accept\n",rank);
 		iter=getIteration(prank);
-		lastnode+=int(pow(2.0,iter));
+		MPI_Recv(buffer,count,datatype, MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 	}
-	while(lastnode<size){
-		printf("I %d send message to %d\n",rank,getRealRank(root,lastnode,size));
-		MPI_Send(buffer,count,datatype,getRealRank(root,lastnode,size),1,MPI_COMM_WORLD);
-		lastnode+=int(pow(2.0,iter++));
+	int lastnode=1<<iter;
+	while(lastnode+prank<size){
+		MPI_Send(buffer,count,datatype,(lastnode+prank+root)%size,1,MPI_COMM_WORLD);
+		lastnode<<=1;
 	}
 	return 0;
 }
-
-char buffer[200]={0};
 int main(int argc, char *argv[]){
 	char buffer[maxBuff];
+	const int root=4;
 	buffer[0]='a';
 	buffer[1]='d';
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
-	if(rank==2)	MyBroadcastSend(buffer,2,MPI_CHAR,rank);
-	else MyBroadcastSend(buffer,2,MPI_CHAR,2);
+	MyBroadcastSend(buffer,2,MPI_CHAR,root);
 	MPI_Finalize();
+	return 0;
 }
